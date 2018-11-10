dc650ef8a809704b89eaed3e9c433be2064b8af2
Andrej <andrej.janda@mail.utoronto.ca>
2018-11-09 20:08:16 -0500

    DriveTrain Cleanup
    - Removed unnecessary member speed variables
    - Moved wall avoidance to seperate functions

--- Robot/DriveTrain.cpp
+++ Robot/DriveTrain.cpp
@@ -8,12 +8,19 @@
 #include "Modules.h"
 
 // Preprocessor definitions
-#define MAX_SPEED 150
-#define DEFAULT_SPEED_R 100
-#define DEFAULT_SPEED_L (0.95f) * DEFAULT_SPEED_R
-#define OBSTACLE_DIST 75.f
-#define WALL_DETECT_DIST 300.f
 #define DEG2RAD 180.f / 3.14f
+
+// Motor Related
+#define MAX_SPEED 150
+#define MOTOR_CALIB 0.95f
+#define DEFAULT_SPEED_R 100
+#define DEFAULT_SPEED_L MOTOR_CALIB * DEFAULT_SPEED_R
+
+// Wall Avoidance Related
+#define OBSTACLE_DIST 75.f
+#define AVOID_DIST 100.f
+#define WALL_AVOID_DIST 40.f
+#define WALL_DETECT_DIST 300.f
 
 DriveTrain::DriveTrain(Motor L, Motor R, UltraSonicArray US, float wheelbase) :
 L(L),
@@ -21,7 +28,6 @@ R(R),
 US(US),
 wheelbase(wheelbase)
 {
-  ResetSpeed();
 }
 
 ErrorCode DriveTrain::Drive()
@@ -53,24 +59,7 @@ ErrorCode DriveTrain::Drive()
       float wallDist;
       while (!isObsticalDetected())
       {
-          float lDist = US.L.getDist();
-          float rDist = US.R.getDist();
-          if (lDist < 40.f)
-          {
-             Stop();
-             Turn(-5);
-             Drive(100, Forward);
-             delay(500);
-             Stop();
-          }
-          if (rDist < 40.f)
-          {
-             Stop();
-             Turn(5);
-             Drive(100, Forward);
-             delay(500);
-             Stop();
-          }
+          AvoidWall();
 
           wallDist = follower->getDist();
           Serial3.println(wallDist);
@@ -79,7 +68,7 @@ ErrorCode DriveTrain::Drive()
               Serial3.println("LOST WALL!!");
               Stop();
               delay(1000);
-              Drive(100, Forward);
+              Drive(DEFAULT_SPEED_R, Forward);
               delay(500);
               Stop();
               if (isLeft)
@@ -90,7 +79,7 @@ ErrorCode DriveTrain::Drive()
               {
                 Turn(-90);
               }
-              Drive(100, Forward);
+              Drive(DEFAULT_SPEED_R, Forward);
               for (auto i = 0; i < 40; ++i)
               {
                 if (isObsticalDetected())
@@ -150,7 +139,6 @@ ErrorCode DriveTrain::FindFollower(UltraSonic*& follower, bool& isLeft)
     }
 
     // Make robot parrallel to wall
-    // MakeWallParallel(follower);
     Serial3.println("FOUND FOLLOWER");  
     return OK;
 }
@@ -191,15 +179,15 @@ ErrorCode DriveTrain::ClearObstacle()
     // Will need to turn to free spot in this case
     if (isObsticalDetected())
     {
-        if (lDist > OBSTACLE_DIST)
+        if (lDist > AVOID_DIST)
         {
           Turn(90.f);
         }
-        else if (rDist > OBSTACLE_DIST)
+        else if (rDist > AVOID_DIST)
         {
           Turn(-90.f);
         }
-        else if (bDist > OBSTACLE_DIST)
+        else if (bDist > AVOID_DIST)
         {
           Turn(180.f);
         }
@@ -218,6 +206,26 @@ bool DriveTrain::isObsticalDetected()
     return US.F.getDist() < OBSTACLE_DIST;
 }
 
+void DriveTrain::AvoidWall()
+{
+    AvoidWall(US.L, true);  // Avoid left wall
+    AvoidWall(US.R, false); // Avoid right wall
+}
+
+void DriveTrain::AvoidWall(UltraSonic& DistSensor, bool isLeft)
+{
+    float dist = DistSensor.getDist();
+    float angle = isLeft ? -5 : 5;
+    if (dist < WALL_AVOID_DIST)
+    {
+        Stop();
+        Turn(angle);
+        Drive(DEFAULT_SPEED_R, Forward);
+        delay(500);
+        Stop();
+    }
+}
+
 void DriveTrain::Drive(int vel, Direction d)
 {
   if (isObsticalDetected())
@@ -225,8 +233,8 @@ void DriveTrain::Drive(int vel, Direction d)
     ClearObstacle();
   }
 
-  L.drive(-DEFAULT_SPEED_L);
-  R.drive(-DEFAULT_SPEED_R);
+  R.drive(d * vel);
+  L.drive(d * MOTOR_CALIB * vel);
 }
 
 void DriveTrain::Drive(int vel, int dist, Direction d)
@@ -259,16 +267,9 @@ void DriveTrain::UpdateSpeed(float wallDist, bool isLeft)
   float speedUpdate = kp * p + kd * d;
   speedUpdate = min(speedUpdate, 12);
   speedUpdate = max(speedUpdate, -12);
-
-  Serial3.print("wallDist: ");
-  Serial3.print(wallDist);
-  Serial3.print(" p: ");
-  Serial3.print(p);
-  Serial3.print(" d: ");
-  Serial3.print(d);
-  Serial3.println();
   
   // Update motor speeds based on control input
+  float rSpeed, lSpeed;
   if (isLeft)
   {
       rSpeed = DEFAULT_SPEED_R - speedUpdate;
@@ -290,12 +291,6 @@ void DriveTrain::UpdateSpeed(float wallDist, bool isLeft)
 
   L.drive(-lSpeed);
   R.drive(-rSpeed);
-}
-
-void DriveTrain::ResetSpeed()
-{
-  rSpeed = DEFAULT_SPEED_R;
-  lSpeed = DEFAULT_SPEED_L;
 }
 
 void DriveTrain::Turn(float angle)
