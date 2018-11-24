904b12f76fefce0fb68791efc81a874033ccb068
cissyyao <45318021+cissyyao@users.noreply.github.com>
2018-11-24 13:54:12 -0500

    Add files via upload

---
+++ example_input.ino
@@ -0,0 +1,17 @@
+int test_row1[] = {1, 2, 3, 4};
+char test_row2[4] = {'w', 'a', 's', 'd'};
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+void loop() {
+  for (int i=0; i < 4; i++)
+  {
+    Serial.println(test_row1[i]);
+    Serial.println(test_row2[i]);
+    delay(1000);
+  }
+  Serial.end();
+  while(true);
+}
